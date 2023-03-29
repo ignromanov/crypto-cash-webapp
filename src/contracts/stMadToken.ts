4cbc2cd76bda3d71d7ed2b0aca33ab1f5ac03a6f
Ignat <romign@gmail.com>
2023-03-29 14:12:37 +0000

    add contracts definition

---
+++ src/contracts/stMadToken.ts
@@ -0,0 +1,11 @@
+import { ethers, JsonRpcProvider } from "ethers";
+import { StMadToken } from "../../../backend/types/contracts/StMadToken";
+import StMadTokenJsonArtifact from "../../../backend/artifacts/contracts/stMadToken.sol/stMadToken.json";
+
+const stMadTokenContractAddress =
+  process.env.NEXT_PUBLIC_TOKEN_CONTRACT_ADDRESS || "";
+const stMadTokenContractAbi = StMadTokenJsonArtifact.abi;
+
+export { stMadTokenContractAddress, stMadTokenContractAbi };
+
+export type StMadTokenType = StMadToken;
