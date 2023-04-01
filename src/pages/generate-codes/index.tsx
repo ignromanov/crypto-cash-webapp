97f1b6c3964d51340a8438c8d93520f0737995dc
Ignat <romign@gmail.com>
2023-04-01 23:19:55 +0000

    add AdminAlert component

--- src/pages/generate-codes/index.tsx
+++ src/pages/generate-codes/index.tsx
@@ -1,9 +1,11 @@
+import { AdminAlert } from "@/components/elements/AdminAlert";
 import { DisplayCodes } from "@/components/modules/DisplayCodes";
 import { GenerateCodes } from "@/components/modules/GenerateCodes";
 
 export default () => {
   return (
     <>
+      <AdminAlert />
       <GenerateCodes />
       <DisplayCodes />
     </>
